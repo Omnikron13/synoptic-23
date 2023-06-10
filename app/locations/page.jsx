76010231b351ea0d9aa45ed31825ecfc499477bd
Joey Sabey <joey.sabey@gmx.com>
2023-06-10 04:18:11 +0100

    add test /locations route - prints out the locations table from the db

---
+++ app/locations/page.jsx
@@ -0,0 +1,30 @@
+import db from '/db.js';
+
+export const metadata = {
+   title: 'Locations Page Title',
+};
+
+
+function Location({ props }) {
+   return(
+      <div>
+         <h2>{props.name}</h2>
+         <p>{props.description}</p>
+         <p>{props.lat}, {props.long}</p>
+      </div>
+   );
+}
+ 
+
+export default async function Page() {
+   const res = await db.query('SELECT * FROM locations', []);
+   // TODO: error handling
+
+   return <>
+      <h1>Locations</h1>
+      {res.rows.map(l =>
+         <Location props={l} />
+      )}
+   </>;
+}
+
