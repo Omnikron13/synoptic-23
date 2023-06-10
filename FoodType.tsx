921dbb05d6df8bbb96667e01511be8bce48e8a29
Joey Sabey <joey.sabey@gmx.com>
2023-06-10 21:49:01 +0100

    add FoodType.tsx

---
+++ FoodType.tsx
@@ -0,0 +1,32 @@
+import classnames from 'classnames';
+import db from '/db.js';
+
+function construct(obj): FoodType {
+   return {
+      id: obj.id,
+      name: obj.name,
+      description: obj.description,
+      classes: obj.classes,
+   };
+}
+
+export async function get(id: number) {
+   const res = await db.query('SELECT * FROM food_types WHERE id = $1', [id]);
+   // TODO: error handling
+   return construct(res.rows[0]);
+}
+
+export async function getAll(): Promise<FoodType[]> {
+   const res = await db.query('SELECT * FROM food_types');
+   return res.rows.map(ft => construct(ft));
+}
+
+export function FoodType({ name, description, classes }) {
+   return (
+      <details className={classnames('foodType', classes)}>
+         <summary>{name}</summary>
+         <p>{description}</p>
+      </details>
+   );
+}
+
