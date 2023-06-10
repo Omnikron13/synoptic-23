2d3fc2fa04e3dc9b7ea9b6882aea45464c220faf
Joey Sabey <joey.sabey@gmx.com>
2023-06-10 06:51:22 +0100

    add super rough draft for Location model class

---
+++ Location.ts
@@ -0,0 +1,30 @@
+import db from '/db.js';
+
+async function construct(obj: Location): Location {
+   // Return (raw) time records for this location
+   obj.getTimes = async () => {
+      const res = await db.query('SELECT * FROM times WHERE location = $1', [obj.id]);
+      // TDOD: error handling
+      return res.rows;
+   };
+
+   // Attach time records to object
+   obj.times = await obj.getTimes();
+
+   return obj;
+}
+
+export async function get(id: number): Promise<Location> {
+   const res = await db.query('SELECT * FROM locations WHERE id = $1', [id]);
+
+   console.log('this from Location.get:', this);
+
+   return construct(res.rows[0]);
+}
+
+export async function getAll(): Promise<Location[]> {
+   const res = await db.query('SELECT * FROM locations');
+
+   return Promise.all(res.rows.map(l => construct(l)));
+}
+
