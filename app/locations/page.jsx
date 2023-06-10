4ba278c1898a83c3ef1c9ff88b77b2c336b74e2a
Joey Sabey <joey.sabey@gmx.com>
2023-06-10 06:52:21 +0100

    rework prototype Location/Times components/test-page to render from model

--- app/locations/page.jsx
+++ app/locations/page.jsx
@@ -1,30 +1,56 @@
 import db from '/db.js';
+import * as model from '/Location.ts';
 
 export const metadata = {
    title: 'Locations Page Title',
 };
 
 
-// TOOO: relocate this, if used at all, to a common location
-function Location({ props }) {
+// TOOO: relocate this, if used at all, to a common location...
+async function Location({ name, description, lat, long, times }) {
    return(
-      <div>
-         <h2>{props.name}</h2>
-         <p>{props.description}</p>
-         <p>{props.lat}, {props.long}</p>
+      <div class='location'>
+         <h2>{name} ({lat}, {long})</h2>
+         {description ?
+            <p>{description}</p>
+            :<></>}
+         <Times times={times} />
       </div>
    );
 }
- 
+
+
+async function Times({ times }) {
+   // TODO: offload this properly elsewhere...
+   const days = { 0: 'Sun', 1: 'Mon', 2: 'Tue', 3: 'Wed', 4: 'Thu', 5: 'Fri', 6:'Sat' };
+
+   if(times.length == 0)
+      return <></>;
+
+   return <>
+      <h3>Times</h3>
+      <ol>
+         {times.map(t =>
+            <li>{days[t.day]} - {t.open} - {t.close}</li>
+         )}
+      </ol>
+   </>;
+}
+
 
 export default async function Page() {
-   const res = await db.query('SELECT * FROM locations', []);
-   // TODO: error handling
+   const locs = await model.getAll();
 
    return <>
       <h1>Locations</h1>
-      {res.rows.map(l =>
-         <Location props={l} />
+      {locs.map(l =>
+         <Location
+            name={l.name}
+            description={l.description}
+            lat={l.lat}
+            long={l.long}
+            times={l.times}
+         />
       )}
    </>;
 }
