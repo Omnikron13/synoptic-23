5041e51d624c41dde8220470e45e7c59e36128ba
Joey Sabey <joey.sabey@gmx.com>
2023-06-12 00:15:36 +0100

    update Location factories to pull from locations_view

--- Location.ts
+++ Location.ts
@@ -1,27 +1,17 @@
 import db from '/db.js';
 
 async function construct(obj: Location): Promise<Location> {
-   // Return (raw) time records for this location
-   obj.getTimes = async () => {
-      const res = await db.query('SELECT day, open, close FROM times WHERE location = $1', [obj.id]);
-      // TDOD: error handling
-      return res.rows;
-   };
-
-   // Attach time records to object
-   obj.times = await obj.getTimes();
-
    return obj;
 }
 
 export async function get(id: number): Promise<Location> {
-   const res = await db.query('SELECT * FROM locations WHERE id = $1', [id]);
+   const res = await db.query('SELECT * FROM locations_view WHERE id = $1', [id]);
 
    return construct(res.rows[0]);
 }
 
 export async function getAll(): Promise<Location[]> {
-   const res = await db.query('SELECT * FROM locations');
+   const res = await db.query('SELECT * FROM locations_view');
 
    return Promise.all(res.rows.map(l => construct(l)));
 }
