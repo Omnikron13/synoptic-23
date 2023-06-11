3aa85c5f55d95159d533b2f2062871c91349f079
Joey Sabey <joey.sabey@gmx.com>
2023-06-11 01:07:52 +0100

    set demo/test map bounds more correctly

--- app/map/Map.jsx
+++ app/map/Map.jsx
@@ -22,7 +22,11 @@ function Map({ lat, lng }) {
 
    const onLoad = useCallback(function callback(map) {
       // TODO: not just blindly copy
-      const bounds = new window.google.maps.LatLngBounds({lat, lng});
+      // Rough bounds of the Govan ward area we're interested in
+      const sw = {lat: 55.846051295550666, lng: -4.35666360454672};
+      const ne = {lat: 55.87486951008421, lng: -4.269570716426365};
+
+      const bounds = new window.google.maps.LatLngBounds(sw, ne);
       map.fitBounds(bounds);
 
       setMap(map)
