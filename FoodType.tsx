7a3cf1fd11e85497d2f278f85bd54f1a36516e9d
Joey Sabey <joey.sabey@gmx.com>
2023-06-12 10:10:15 +0100

    rework FoodType to use some of the recent syntatic sugar for class/get/etc.

--- FoodType.tsx
+++ FoodType.tsx
@@ -1,6 +1,27 @@
 import classnames from 'classnames';
 import db from '/db.js';
 
+export default class FoodType {
+   id: string;
+   name: string;
+   description: string;
+   meta: any;
+
+   constructor({ id, name, description, meta, ...r}) {
+      this.id = id;
+      this.name = name;
+      this.description = description;
+      this.meta = meta;
+   }
+
+   // Wrapping this up in the slightly odd getter + lambda combination sidesteps
+   // an issue with 'this' falling out of scope inside React.js/Next.js.
+   // Closures to the rescue once again...
+   get detailsComponent() {
+      return props => detailsComponent({...this, ...props});
+   }
+}
+
 function construct(obj): FoodType {
    return {
       id: obj.id,
@@ -13,12 +34,12 @@ function construct(obj): FoodType {
 export async function get(id: string) : Promise<FoodType> {
    const res = await db.query('SELECT * FROM food_types WHERE id = $1', [id]);
    // TODO: error handling
-   return construct(res.rows[0]);
+   return new FoodType(res.rows[0]);
 }
 
 export async function getAll() : Promise<FoodType[]> {
    const res = await db.query('SELECT * FROM food_types');
-   return res.rows.map(ft => construct(ft));
+   return res.rows.map(ft => new FoodType(ft));
 }
 
 export function detailsComponent({ id, name, description, meta }) {
