f8700c475be6c209109428685b2e08fc28a10c67
Joey Sabey <joey.sabey@gmx.com>
2023-06-13 12:32:30 +0100

    rework Location to be a 'true' class

--- Location.tsx
+++ Location.tsx
@@ -1,20 +1,35 @@
+import classnames from 'classnames';
 import db from 'db';
-import FoodType from '/FoodType.tsx';
+import FoodType from 'FoodType';
 
-async function construct(obj: Location): Promise<Location> {
-   obj.food_types = obj.food_types.map(ft => new FoodType(ft));
-   return obj;
+export default class Location {
+   id: number;
+   name: string;
+   description: string;
+   coords: { lat: number, lng: number };
+   times: { day: string, open: string, close: string }[];
+   food_types: FoodType[];
+
+   constructor({ id, name, description, coords, times, food_types, ...r}) {
+      this.id = id;
+      this.name = name;
+      this.description = description;
+      this.coords = coords;
+      this.times = times;
+      this.food_types = food_types.map(ft => new FoodType(ft));
+   }
 }
 
-export async function get(id: number): Promise<Location> {
+
+export async function get(id: number) : Promise<Location> {
    const res = await db.query('SELECT * FROM locations_view WHERE id = $1', [id]);
 
-   return construct(res.rows[0]);
+   return new Location(res.rows[0]);
 }
 
-export async function getAll(): Promise<Location[]> {
+export async function getAll() : Promise<Location[]> {
    const res = await db.query('SELECT * FROM locations_view');
 
-   return Promise.all(res.rows.map(l => construct(l)));
+   return res.rows.map(l => new Location(l));
 }
 
