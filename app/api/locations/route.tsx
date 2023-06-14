5bc92b898bd3420c9cd1f5027e4852cf9536d87b
Joey Sabey <joey.sabey@gmx.com>
2023-06-14 22:00:14 +0100

    add /api/locations endpoint, can fetch locations data raw or sorted by distance from given coords

---
+++ app/api/locations/route.tsx
@@ -0,0 +1,28 @@
+import { NextResponse } from 'next/server';
+import { getAll } from 'Location';
+
+export async function GET(req) {
+   let data = await getAll();
+   if(req.nextUrl.searchParams.get('start')) {
+      const matrix = await fetchDistanceMatrix(req.nextUrl.searchParams.get('start'), data);
+      console.log('MATRIX:', matrix.rows[0].elements);
+      data = data
+         .map((l, i) => ({...l, distance: matrix.rows[0].elements[i].distance}))
+         .sort((a, b) => a.distance.value - b.distance.value);
+   }
+   return NextResponse.json(data);
+}
+
+async function fetchDistanceMatrix(userCoords, locations) {
+   const apiKey = 'AIzaSyCXhK5ERNDL42X1HnIj8gFgMeNr0iKERiI';
+   const url = 'https://maps.googleapis.com/maps/api/distancematrix/json?'
+   const params = new URLSearchParams({
+      destinations: locations.map(l => `${l.coords.lat},${l.coords.lng}`).join('|'),
+      origins: userCoords,
+      units: 'metric',
+      key: apiKey,
+   });
+   console.log('URL PARAMS:', params.toString());
+   const res = await fetch(url + params);
+   return res.json();
+}
